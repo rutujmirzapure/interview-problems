d49de8afa2959bf6814340dcbcd3f03e174b3157
rutuj mirzapure <44692465+rutujmirzapure@users.noreply.github.com>
2019-11-23 11:55:24 +0530

    Add files via upload
    ques from thinking in java

---
+++ TestFirstApp.java
@@ -0,0 +1,9 @@
+package mcqs;
+
+public class TestFirstApp {
+	
+	public static void main(String[] args) {
+		System.out.println((1==1)?(7==8)?5:6:4);
+	}
+
+}
---
+++ TestFirstApp2.java
@@ -0,0 +1,11 @@
+package mcqs;
+
+public class TestFirstApp2 {
+	
+	public static void main(String[] args) {
+		int wer;
+		System.out.println("the value of wer is :"+ wer);
+	}
+
+}
+ 
